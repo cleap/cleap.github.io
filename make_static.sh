8b8a89d0e7b38e30762fca82afc503a66eea129c
Christopher Leap <cleapdev@gmail.com>
2021-11-10 21:48:21 -0700

    Removed 'index.html' from nav paths

--- make_static.sh
+++ make_static.sh
@@ -8,3 +8,7 @@ rm index.html
 
 echo "Downloading static site"
 wget -k -E -r -l 10 -p -N -F -nH localhost
+
+echo "Removing 'index.html' from paths"
+sed -i -e 's/index.html//g' index.html
+sed -i -e 's/index.html//g' */index.html
